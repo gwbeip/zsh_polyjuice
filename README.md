0c784d712e92d3848bc8603d95c247c9438c2b25
gwbeip <doregist@yeah.net>
2023-03-30 16:23:24 +0800

    feat(all):automatically configure zsh
    based on the oh-my-zsh project

---
+++ README.md
@@ -0,0 +1,14 @@
+# ZSH Polyjuice
+
+Automatically configure [ZSH](https://github.com/zsh-users/zsh) based on [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh) according to my habits. 
+
+## The `Polyjuice` theme
+The theme `polyjuice.zsh-theme` is based the [ys](https://github.com/ohmyzsh/ohmyzsh/blob/master/themes/ys.zsh-theme) theme.
+
+## Usage
+Install `ZSH` according the the platform you use. Clone this repository and run `polyjuice.sh`.
+
+```shell
+git clone https://github.com/gwbeip/zsh_polyjuice.git $HOME/.zsh_polyjuice
+bash $HOME/.zsh_polyjuice/polyjuice.sh
+```
---
+++ polyjuice.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/plugins/zsh-autocomplete ]; then
+    git clone https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.oh-my-zsh/plugins/zsh-autocomplete
+fi
+
+if [ -f $HOME/.zshrc ]; then
+    if [ ! -f $HOME/.zshrc.bkp ]; then
+        mv $HOME/.zshrc $HOME/.zshrc.bkp
+    fi
+fi
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+
+zsh_theme_line_num=$(grep -rn -m 1 ZSH_THEME= $HOME/.zshrc | cut -d ":" -f 1)
+sed -i $zsh_theme_line_num"c"\ZSH_THEME=\"polyjuice\"  $HOME/.zshrc
+
+zsh_plugin_line_number=$(grep -rn -m 2 plugins=\(git\)  $HOME/.zshrc | cut -d : -f 1)
+sed -i "$zsh_plugin_line_number"c"\plugins=\(zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete\)"  $HOME/.zshrc
+
+if [ -f $HOME/.oh-my-zsh/themes/polyjuice.zsh-theme ]; then 
+    rm -rf $HOME/.oh-my-zsh/themes/polyjuice.zsh-theme
+fi
+ln -i -s $(pwd)/polyjuice.zsh-theme $HOME/.oh-my-zsh/themes/polyjuice.zsh-theme
